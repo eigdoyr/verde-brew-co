6ac3a9490e0949e45f1dcb73757241a6ca5fc637
Ryodgie Barnatia <ryodgie@gmail.com>
2026-04-15 06:55:37 +0800

    chore: use native Vite tsconfig paths resolution

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    tsconfigPaths: true,
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
