caea30466bd2de06685775aa0fb517bc7c535928
Ryodgie Barnatia <ryodgie@gmail.com>
2026-04-14 11:16:00 +0800

    chore: scaffold project and set up design tokens

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main>
+      <h1
+        style={{
+          padding: "2rem",
+          fontFamily: "var(--font-body)",
+          fontSize: "3rem",
+        }}
+      >
+        Verde Brew Co.
+      </h1>
+    </main>
+  );
+}
+
+export default App;
